4ba62a28d00b28877d5477bb700c9c1c9748029e
omarmohamed101 <omar.mohamed5557@gmail.com>
2021-09-07 23:30:20 +0200

    model with 94% acc

--- traffic.py
+++ traffic.py
@@ -78,9 +78,36 @@ def get_model():
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
     # Create a neural network
-    model = tf.keras.models.Sequential()
+    model = tf.keras.models.Sequential([
+        # Convolutional layer. Learn 32 filters using a 3x3 kernel
+        tf.keras.layers.Conv2D(
+            50, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
 
-    # TO BE DONE
+        # Max-pooling layer, using 2x2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+
+        # Flatten units
+        tf.keras.layers.Flatten(),
+
+        # Add a hidden layer with dropout
+        tf.keras.layers.Dense(150, activation="relu"),
+        tf.keras.layers.Dense(150, activation="relu"),
+        tf.keras.layers.Dense(150, activation="relu"),
+
+        tf.keras.layers.Dropout(0.5),
+
+        # Add an output layer with output units for all 10 digits
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+    ])
+
+    # Train neural network
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
     
     return model
 
