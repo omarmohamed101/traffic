882f8a937e27a4505156f4504ffd43866ddbc505
omarmohamed101 <omar.mohamed5557@gmail.com>
2021-09-14 20:19:26 +0200

    97%

--- traffic.py
+++ traffic.py
@@ -81,20 +81,29 @@ def get_model():
     model = tf.keras.models.Sequential([
         # Convolutional layer. Learn 32 filters using a 3x3 kernel
         tf.keras.layers.Conv2D(
-            50, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+            64, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
         ),
 
         # Max-pooling layer, using 2x2 pool size
         tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
 
+        tf.keras.layers.Conv2D(
+            64, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
+
+        # Max-pooling layer, using 2x2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        
+
+
+
         # Flatten units
         tf.keras.layers.Flatten(),
 
         # Add a hidden layer with dropout
-        tf.keras.layers.Dense(150, activation="relu"),
-        tf.keras.layers.Dense(150, activation="relu"),
-        tf.keras.layers.Dense(150, activation="relu"),
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dense(128, activation="relu"),
 
         tf.keras.layers.Dropout(0.5),
 
